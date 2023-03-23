b7fbb87f5ca21b2d22513435c6bb1d5e94af5f36
hyudai28 <hayasakayudai@gmail.com>
2023-03-23 19:29:25 +0900

    Output message changed from user name to display name

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -6,7 +6,10 @@ module.exports = {
     if (oldState.channelId !== newState.channelId) {
       if (!oldState.channel && newState.channel) {
         const newUser = await client.users.fetch(newState.member.id);
-        const msg = `${newUser.username}がボイスチャットに入室しました。`;
+        const msg =
+          ":blue_circle: " +
+          newState.member.displayName +
+          " が通話を開始しました :blue_circle:";
         const server = getChannelId(newState.guild);
 
         if (server.channelId == client.config.channelId) {
@@ -16,7 +19,10 @@ module.exports = {
         const newUser = await client.users.fetch(oldState.member.id);
         const server = getChannelId(newState.guild);
         if (server.channelId == client.config.channelId) {
-          const msg = `${newUser.username}が通話を終了しました。`;
+          const msg =
+            ":red_circle: " +
+            newState.member.displayName +
+            " が通話を終了しました :red_circle:";
           client.channels.cache.get(client.config.channelId).send(msg);
         }
       }
